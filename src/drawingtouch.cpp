daec5e2ad5ea2b7e226c9c0b3836f17c3699d7a7
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2012-11-02 17:07:36 +0100

    fix missing LICENSE + README

--- src/drawingtouch.cpp
+++ src/drawingtouch.cpp
@@ -1,3 +1,21 @@
+/**	mtdiag, a multitouch diagnostic tool
+ **
+ **	The copyright owners for the contents of this file are:
+ **		Ecole Nationale de l'Aviation Civile, France (2011-2012)
+ **
+ **	this files contains a subclass of Touch
+ **
+ **	Contributors:
+ **		Benjamin Tissoires <benjamin.tissoires@enac.fr>
+ **
+ **
+ **	This program is provided to you as free software;
+ **	you can redistribute it	and/or modify it under the terms of the
+ **	GNU General Public License as published by the Free Software
+ **	Foundation; either version 2 of the License, or (at your option)
+ **	any later version.
+ **/
+
 #include "drawingtouch.h"
 
 DrawingTouch::DrawingTouch(QGraphicsScene *scene, QObject *parent) :
